c2c0cedca2bbcb82fd5b8899e2dd3d2b691f00ca
Julian Rocci <rocci.julian@gmail.com>
2026-04-02 19:11:31 +0200

    Update stg_wh_metering on monthly

--- models/staging/stg_warehouse_metering.sql
+++ models/staging/stg_warehouse_metering.sql
@@ -4,20 +4,28 @@
 
 with raw_metering as (
     select * from {{ source('snowflake_usage', 'warehouse_metering_history') }}
+    -- We filter here monthly
+    where start_time >= dateadd(day, -30, current_timestamp())
 ),
 
 final as (
     select
-        -- Using MD5 or Hash for a surrogate key is a dbt best practice
+        -- Surrogate Key (Unique ID for dbt tests)
         md5(cast(concat(warehouse_name, start_time) as string)) as metering_id,
         warehouse_name,
         start_time,
         end_time,
+        
+        -- Duration of the billing window in seconds
+        datediff('second', start_time, end_time) as billed_seconds,
+        
+        -- All credit consumption
         credits_used,
         credits_used_compute,
         credits_used_cloud_services,
-        -- Random reference price (can be parameterized with a variable later)
-        4.0 as credit_price_usd 
+        
+        -- Credit price (standard for example)
+        4.0 as credit_price_usd
     from raw_metering
 )
 
